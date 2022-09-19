ef3d08ea6fa4fec62a0b6e875053001aca4314c6
Kevin Castillo <kevincastillo318@gmail.com>
2022-09-19 07:06:09 -0700

    update data

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -15,11 +15,10 @@ export default function Recipe() {
   useEffect(() => {
     setIsPending(true);
 
-    projectFirestore
+    const unsub = projectFirestore
       .collection("recipes")
       .doc(id)
-      .get()
-      .then((doc) => {
+      .onSnapshot((doc) => {
         if (doc.exists) {
           setIsPending(false);
           setRecipe(doc.data());
@@ -28,7 +27,16 @@ export default function Recipe() {
           setError("Recipe not found");
         }
       });
+    return () => {
+      unsub();
+    };
   }, [id]);
+
+  // const handleClick = () => {
+  //   projectFirestore.collection("recipes").doc(id).update({
+  //     title: "something different",
+  //   });
+  // };
 
   return (
     <div className={`recipe ${mode}`}>
@@ -44,6 +52,7 @@ export default function Recipe() {
             ))}
           </ul>
           <p className="method">{recipe.method}</p>
+          {/* <button onClick={handleClick}>Update</button> */}
         </>
       )}
     </div>
